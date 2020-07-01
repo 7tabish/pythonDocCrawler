8b93bf16d46d5a46c8c583095b1d169c38565487
Tabish Manzoor <43508735+7tabish@users.noreply.github.com>
2020-07-01 22:23:15 +0500

    add max_depth parameter.

--- crawler.py
+++ crawler.py
@@ -7,31 +7,31 @@ from bs4 import BeautifulSoup as BS
 
 
 class WebGraber:
-    def __init__(self,patterns,start_url,max_depth):
-        self.start_url=start_url
-        self.allowed_patterns=allowed_patterns
-        self.max_depth=max_depth
-        self.all_urls=set([])
-        self.sub_level_urls=set([])
-        self.temp_urls=set([])
+    def __init__(self, patterns, start_url, max_depth):
+        self.start_url = start_url
+        self.allowed_patterns = allowed_patterns
+        self.max_depth = max_depth
+        self.all_urls = set([])
+        self.sub_level_urls = set([])
+        self.temp_urls = set([])
 
     def crawl(self):
         self.all_urls.add(self.start_url)
-        if self.max_depth==0:
+        if self.max_depth == 0:
             self.url_to_disk()
             return
-        
-        #section run if max_depth are more than 0
+
+        # section run if max_depth are more than 0
         self.sub_level_urls.add(self.start_url)
         initial_depth = 1
-        #self.sub_levels_urls are to save the next level urls of current page
-        while initial_depth <self.max_depth+ 1:
-            #self.temp_urls are use to save temporary next levels urls and when iteration of sub_urls complete then we set new ursl from self.temp_url to self.sub_level_url
-            self.temp_urls= set([])
+        # self.sub_levels_urls are to save the next level urls of current page
+        while initial_depth < self.max_depth + 1:
+            # self.temp_urls are use to save temporary next levels urls and when iteration of sub_urls complete then we set new ursl from self.temp_url to self.sub_level_url
+            self.temp_urls = set([])
             print("Level ", initial_depth)
             for sub_url in self.sub_level_urls:
                 print(" \nVisiting URL =>", sub_url)
-                response = download_url(sub_url)
+                response = self.download_url(sub_url)
                 # fething urls (href) from the visiting page (sub_url)
                 for url in response.find_all("a"):
                     fetch_url = url.get("href")
@@ -55,7 +55,7 @@ class WebGraber:
         print("/n Extract url and save to disk")
         self.url_to_disk()
 
-    def add_url(self,url):
+    def add_url(self, url):
         # filter allowed urls
         if self.allowed_urls(url):
             # add url to temp_url set only if that url is unvisit
@@ -63,10 +63,11 @@ class WebGraber:
                 print(" - sub url => ", url)
                 self.temp_urls.add(url)
             self.all_urls.add(url)
-    #extract all level urls in self.all_urls and save it to file
+
+    # extract all level urls in self.all_urls and save it to file
     def url_to_disk(self):
         for url in self.all_urls:
-            #base_url can be self.start_url then you will only ge result of specific version that is in your self.start_url
+            # base_url can be self.start_url then you will only ge result of specific version that is in your self.start_url
             base = "https://docs.python.org/"
             _url = url.replace(base, "")
             dir = _url
@@ -90,42 +91,35 @@ class WebGraber:
 
             print("Fecthing => ", url)
             res = self.download_url(url)
-            print(" Filepath => ",FILEPATH)
+            print(" Filepath => ", FILEPATH)
             with open(FILEPATH, "w", encoding="utf-8") as file:
                 file.write(str(res))
 
-    def download_url(self,url):
+    def download_url(self, url):
         response = requests.get(url)
         if response.status_code != 200:
             print("Error while extracting url. Please check your url and version number")
             sys.exit()
         return BS(response.text, 'html.parser')
 
-    def allowed_urls(self,url):
+    def allowed_urls(self, url):
         for pattern in self.allowed_patterns:
             if pattern.search(url):
                 return True
         return False
 
-def download_url(url):
-    response = requests.get(url)
-    if response.status_code != 200:
-        print("Error while extracting url. Please check your url and version number")
-        sys.exit()
-    return BS(response.text, 'html.parser')
 
 
 
-url_pattern1="^(https://docs.python.org)/[0-9](.[0-9])/$"
-url_pattern2="^(https://docs.python.org)/[0-9](.[0-9])/[A-Za-z0-9-.]+/?[a-zA-z0-9.-]*$"
-url_pattern3="^(https://docs.python.org)/[0-9](.[0-9])/[A-Za-z0-9.-]+[/][A-Za-z0-9.-]+/[A-Za-z0-9.-]*$"
+url_pattern1 = "^(https://docs.python.org)/[0-9](.[0-9])/$"
+url_pattern2 = "^(https://docs.python.org)/[0-9](.[0-9])/[A-Za-z0-9-.]+/?[a-zA-z0-9.-]*$"
+url_pattern3 = "^(https://docs.python.org)/[0-9](.[0-9])/[A-Za-z0-9.-]+[/][A-Za-z0-9.-]+/[A-Za-z0-9.-]*$"
 
-url_pattern1=re.compile(url_pattern1)
-url_pattern2=re.compile(url_pattern2)
-url_pattern3=re.compile(url_pattern3)
-allowed_patterns=[url_pattern1,url_pattern2,url_pattern3]
-
+url_pattern1 = re.compile(url_pattern1)
+url_pattern2 = re.compile(url_pattern2)
+url_pattern3 = re.compile(url_pattern3)
+allowed_patterns = [url_pattern1, url_pattern2, url_pattern3]
 
 if __name__ == '__main__':
-    webgraber=WebGraber(allowed_patterns,"https://docs.python.org/3.7/",2)
+    webgraber = WebGraber(allowed_patterns, "https://docs.python.org/3.7/", 2)
     webgraber.crawl()
