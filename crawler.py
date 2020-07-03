6010bea1359da73c576ea2b73230a53a9eb19673
Tabish Manzoor <43508735+7tabish@users.noreply.github.com>
2020-07-03 08:59:20 +0500

    update code for filepath

--- crawler.py
+++ crawler.py
@@ -4,12 +4,12 @@ import re
 import copy
 import os
 from bs4 import BeautifulSoup as BS
-
+from urllib.parse import urljoin,urlparse
 
 class WebGraber:
-    def __init__(self, patterns, start_url, max_depth):
+    def __init__(self, allowed_domains, start_url, max_depth):
         self.start_url = start_url
-        self.allowed_patterns = allowed_patterns
+        self.allowed_domains = allowed_domains
         self.max_depth = max_depth
         self.all_urls = set([])
         self.sub_level_urls = set([])
@@ -36,90 +36,76 @@ class WebGraber:
                 for url in response.find_all("a"):
                     fetch_url = url.get("href")
 
-                    if not ".zip" in fetch_url and not ".tar.bz2" in fetch_url and not fetch_url.startswith(
-                            '.') and not fetch_url.startswith("https"):
-                        lastName = sub_url.rsplit("/", 1)[1]
-                        if ".html" in lastName:
-                            base_url = sub_url.rsplit("/", 1)[0]
-                            fetch_url = base_url + "/" + fetch_url
-                        else:
-                            fetch_url = sub_url + fetch_url
-
+                    if not "archives" in fetch_url:
+                        fetch_url=urljoin(sub_url,fetch_url)
                         self.add_url(fetch_url)
 
-                    if fetch_url.startswith("https"):
-                        self.add_url(fetch_url)
+
 
             self.sub_level_urls = copy.deepcopy(self.temp_urls)
             initial_depth = initial_depth + 1
-        print("/n Extract url and save to disk")
+        print("/n Extract url and save to disk", len(self.all_urls))
         self.url_to_disk()
 
     def add_url(self, url):
         # filter allowed urls
-        if self.allowed_urls(url):
+        if self.check_allowed_domains(url):
             # add url to temp_url set only if that url is unvisit
             if url not in self.all_urls:
-                print(" - sub url => ", url)
                 self.temp_urls.add(url)
             self.all_urls.add(url)
 
     # extract all level urls in self.all_urls and save it to file
     def url_to_disk(self):
         for url in self.all_urls:
-            # base_url can be self.start_url then you will only ge result of specific version that is in your self.start_url
-            base = "https://docs.python.org/"
-            _url = url.replace(base, "")
-            dir = _url
+            try:
+                parseUrl=urlparse(url)
+                print("Fetching => ",url)
+                response=self.download_url(url)
+                if response:
+                    if url[-1] == "/":
+                        FILEPATH = parseUrl.path[1:-1]+".txt"
+                    else:
+                        FILEPATH = parseUrl.path[1:]+".txt"
 
-            if dir.rsplit("/", 1)[1]:
-                if ".html" in dir.rsplit("/", 1)[1]:
-                    dir = dir.rsplit("/", 1)[0]
+                    if "/" in FILEPATH:
+                        if not os.path.exists(os.path.dirname(FILEPATH)):
+                            os.makedirs(os.path.dirname(FILEPATH))
+                    #else no need to create directory direct file will create in root folder
 
-            if not os.path.exists(dir):
-                os.makedirs(dir)
-            if _url.rsplit("/", 1)[1]:
-                filename = _url.rsplit("/", 1)[1]
-            else:
-                filename = dir.rsplit("/", 1)[0]
-            filename = filename + ".txt"
-            # create a filepath
-            if dir[-1] == "/":
-                FILEPATH = dir + filename
-            else:
-                FILEPATH = dir + "/" + filename
-
-            print("Fecthing => ", url)
-            res = self.download_url(url)
-            print(" Filepath => ", FILEPATH)
-            with open(FILEPATH, "w", encoding="utf-8") as file:
-                file.write(str(res))
+                    with open(FILEPATH, "w", encoding="utf-8") as file:
+                        file.write(str(response))
+                    print("Filepath => ",FILEPATH)
+            except Exception as error:
+                    print("error: ",error)
 
     def download_url(self, url):
         response = requests.get(url)
         if response.status_code != 200:
-            print("Error while extracting url. Please check your url and version number")
-            sys.exit()
+            print("Error while extracting url. Please check your url and version number",url)
+            return False
         return BS(response.text, 'html.parser')
 
-    def allowed_urls(self, url):
-        for pattern in self.allowed_patterns:
-            if pattern.search(url):
+    def check_allowed_domains(self, url):
+        for domain in self.allowed_domains:
+            if domain.search(url):
                 return True
         return False
 
 
 
 
-url_pattern1 = "^(https://docs.python.org)/[0-9](.[0-9])/$"
-url_pattern2 = "^(https://docs.python.org)/[0-9](.[0-9])/[A-Za-z0-9-.]+/?[a-zA-z0-9.-]*$"
-url_pattern3 = "^(https://docs.python.org)/[0-9](.[0-9])/[A-Za-z0-9.-]+[/][A-Za-z0-9.-]+/[A-Za-z0-9.-]*$"
+domain1 = "^(https://docs.python.org)/[0-9](.[0-9])/[A-Za-z0-9./-]+$"
+domain2 = "^(https://www.python.org/)[a-zA-z0-9.-/]+$"
 
-url_pattern1 = re.compile(url_pattern1)
-url_pattern2 = re.compile(url_pattern2)
-url_pattern3 = re.compile(url_pattern3)
-allowed_patterns = [url_pattern1, url_pattern2, url_pattern3]
+
+
+allowed_domain1 = re.compile(domain1)
+allowed_domain2 = re.compile(domain2)
+
+
+allowed_domains = [allowed_domain1, allowed_domain2]
 
 if __name__ == '__main__':
-    webgraber = WebGraber(allowed_patterns, "https://docs.python.org/3.7/", 2)
+    webgraber = WebGraber(allowed_domains, "https://docs.python.org/3.7/", 1)
     webgraber.crawl()
